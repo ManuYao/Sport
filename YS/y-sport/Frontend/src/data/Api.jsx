f5449f85e4461492301859dcf97c17dba5599296
Manu Y <manu9494230@gmail.com>
2023-11-29 21:03:09 +0100

    AV 0.2.5 🐇 | InProgress

---
+++ YS/y-sport/Frontend/src/data/Api.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect} from 'react'
+
+export default function Api() {
+    const [dataEvent, setDataEvent] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() =>{
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost/sport/API.php');
+                if (!response.ok){
+                    throw new Error('La requête a échoué');
+                }
+                const data = await response.json();
+
+                setDataEvent(Array.isArray(data) ? data :[]);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false)
+            }
+        };
+
+        fetchData();
+
+    []})
+    console.log(data)
+  return (
+    <div>Api</div>
+  )
+}
