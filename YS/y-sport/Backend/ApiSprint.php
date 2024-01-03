a12094acbe95495a9eeb10bbd973aa75137468f4
ManuYao <manu9494230@gmail.com>
2024-01-03 10:22:47 +0100

    AV 0.3.5 🧶 | + API

---
+++ YS/y-sport/Backend/ApiSprint.php
@@ -0,0 +1,35 @@
+<?php
+/*  -FR-  Cette ligne permet d'autoriser toutes les origines à accéder à la ressource. 
+        (n'importe quelle origine) est autorisé à faire des requêtes au YCulturelles PHP.
+    
+    -EN- This line allows you to authorize all origins to access the resource.
+         (any origin) is authorized to make requests to YCulturelles PHP.
+*/
+header("Access-Control-Allow-Origin: *");
+
+/*  -FR- Cette ligne spécifie le type de contenu que le serveur envoie en réponse.
+     Dans ce cas, elle indique que le contenu est au format JSON (`application/json`) 
+     et utilise l'encodage de caractères UTF-8 (`charset=UTF-8`).
+    
+    -EN- This line specifies the type of content that the server sends in response.
+      In this case, it indicates that the content is in JSON format (`application/json`)
+      and uses UTF-8 character encoding (`charset=UTF-8`).
+*/
+
+/*
+header("Content-Type: application/json; charset=UTF-8");
+
+//requête à l'API OpenAgenda
+$data = file_get_contents('https://data.smartidf.services/api/explore/v2.1/catalog/datasets/osm-fr-sport/records?limit=100&refine=sport%3A%22fitness%22'); 
+$data = json_decode($data, true);
+echo json_encode($data['results']);
+
+*/
+
+header('Content-Type: application/json');
+
+$data = file_get_contents('./APISportSprint.json');
+
+echo $data;
+
+?>
--- YS/y-sport/Frontend/src/data/Api.jsx
+++ YS/y-sport/Frontend/src/data/Api.jsx
@@ -1,49 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet'; 
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import '../styles/Map.scss'
-import MarkerClusterGroup from 'react-leaflet-cluster'
+import '../styles/Map.scss';
+import MarkerClusterGroup from 'react-leaflet-cluster';
 
 export default function ApiMap() {
-  //State
   const [dataEvent, setDataEvent] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  //Appel API
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost/YSport/API.php');
-        if (!response.ok) {
-          throw new Error('La requête a échoué');
-        }
-        const data = await response.json();
-
-        setDataEvent(Array.isArray(data) ? data : []);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
+  const fetchData = async (apiUrl) => {
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('La requête a échoué !');
       }
-    };
+      const data = await response.json();
+      setDataEvent(prevData => [...prevData, ...(Array.isArray(data) ? data : [])]);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    fetchData();
+  useEffect(() => {
+    fetchData('http://localhost/YSport/ApiWorkout.php');
+    fetchData('http://localhost/YSport/Apiskate.php');
+    fetchData('http://localhost/YSport/ApiBasket.php');
+    fetchData('http://localhost/YSport/ApiSprint.php');
+    fetchData('http://localhost/YSport/ApiNation.php');
   }, []);
 
-  // objet d'icône 
   const customIcon = new L.Icon({
     iconUrl: '../images/élongation.png',
-    iconSize: [32, 32], 
-    iconAnchor: [16, 32], 
-    popupAnchor: [0, -32], 
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
   });
 
   return (
     <div className='map'>
-      {loading && <p>Chargement...</p>}
-      {error && <p>Erreur: {error}</p>}
+      {loading && <p>Chargement...👌</p>}
+      {error && <p>Erreur:😒 {error}</p>}
 
       {dataEvent.length > 0 && (
         <MapContainer center={[48.7882752, 2.3232512]} zoom={13} className='map_map'>
@@ -51,25 +51,21 @@ export default function ApiMap() {
             url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
             attribution="<a href='https://carto.com/attribution'>Carto</a> [^5^][5]"
           />
-          <MarkerClusterGroup chunkedLoading >
-          {dataEvent.map((event) => {
-            const lat = parseFloat(event.geo_point_2d.lat);
-            const lon = parseFloat(event.geo_point_2d.lon);
+          <MarkerClusterGroup chunkedLoading>
+            {dataEvent.map((event) => {
+              const lat = parseFloat(event.geo_point_2d.lat);
+              const lon = parseFloat(event.geo_point_2d.lon);
 
-            if (!isNaN(lat) && !isNaN(lon)) {
-              return (
-                <Marker
-                  key={event.osm_id}
-                  position={[lat, lon]}
-                  icon={customIcon}
-                >
-                  <Popup>{event.name}</Popup>
-                </Marker>
-              );
-            }
+              if (!isNaN(lat) && !isNaN(lon)) {
+                return (
+                  <Marker key={event.osm_id} position={[lat, lon]} icon={customIcon}>
+                    <Popup>{event.name}</Popup>
+                  </Marker>
+                );
+              }
 
-            return null;
-          })}
+              return null;
+            })}
           </MarkerClusterGroup>
         </MapContainer>
       )}
