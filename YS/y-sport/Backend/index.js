2a7a8bad0ea88e019514d7d573d21c29672e7036
Manu Y <manu9494230@gmail.com>
2024-04-28 02:44:14 +0200

    Va0.0.6 ⚡️ |  Surveillance data aea

--- YS/y-sport/Backend/index.js
+++ YS/y-sport/Backend/index.js
@@ -9,6 +9,7 @@ const APIFitness = require("./APISportFiteness");
 const APINatation = require("./APISportNation");
 const APISkateboard = require("./APISportSkate");
 const APISprint = require("./APISportSprint");
+const createSportModel = require("./models/SportModel");
 
 const app = express();
 
@@ -18,22 +19,26 @@ app.use(express.json());
 mongoose.connect("mongodb://127.0.0.1:27017/ysport", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+}).then(async () => {
     console.log('Connexion à la base de données réussie');
     // Injection des sports dans la base de données
-    const createSportModel = require("./models/SportModel");
 
     const injectSports = async (sports, sportName) => {
         try {
             // Crée un modèle spécifique pour chaque sport
             const Sport = createSportModel(sportName);
-            for (const sport of sports) {
-                // Convertit la valeur de wheelchair en boolean
-                const wheelchair = sport.wheelchair === "limited" ? true : false;
-                await Sport.create({ ...sport, wheelchair });
-                console.log(`${sportName} injecté`);
+            const existingSports = await Sport.find();
+            if (existingSports.length === 0) {
+                for (const sport of sports) {
+                    // Convertit la valeur de wheelchair en boolean
+                    const wheelchair = sport.wheelchair === "limited" ? true : false;
+                    await Sport.create({ ...sport, wheelchair });
+                    console.log(`${sportName} injecté`);
+                }
+                console.log("Injection terminée");
+            } else {
+                console.log(`Les sports ${sportName} existent déjà dans la base de données`);
             }
-            console.log("Injection terminée")
         } catch (error) {
             console.error(`Une erreur est survenue lors de l'injection des sports ${sportName}: `, error);
         }
@@ -47,6 +52,19 @@ mongoose.connect("mongodb://127.0.0.1:27017/ysport", {
 }).catch((error) => {
     console.error('Erreur lors de la connexion à la base de données 🟠 :', error);
 });
+
+// // Récupération de tous les sports 🚧
+// app.get("/sports", async (req, res) => {
+//     const sports = await Promise.all([
+//         APIFitness.find(),
+//         APIBasketball.find(),
+//         APINatation.find(),
+//         APISkateboard.find(),
+//         APISprint.find(),
+//     ]);
+
+//     res.json(sports.flat());
+// });
 
 app.listen(3000, () => {
     console.log("Serveur démarré sur le port 3000");
