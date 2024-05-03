0c30a51f8e3fad9b75bcecec5f401ef926ecf170
Manu Y <manu9494230@gmail.com>
2024-05-03 18:06:33 +0200

    Va.0.1.2 💄🚧 | Home Page basic

---
+++ YS/client/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+export default function Home() {
+  return (
+    <div>
+        <p>Y</p>
+        <a href='#'>Lorem</a>
+
+        <div>
+            <p>Réseau</p>
+            <img src='#' alt='#'></img>
+            <img src='#' alt='#'></img>
+            <img src='#' alt='#'></img>
+        </div>
+
+        <div>
+            <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+            <h2>Ut enim ad minim veniam, quis nostrud</h2>
+            <Button variant='contained' endIcon={"#"}>f</Button>
+        </div>
+    </div>
+  )
+}
