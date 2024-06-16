cbca4d98cb03bc4dba0bb113e79f10d9acb06de5
Manu Y <manu9494230@gmail.com>
2024-06-17 00:12:53 +0200

    Va.0.1.7 💄🚧 | Home Static

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import '../styles/page/home.scss'
 
 export default function Home() {
+
+  const onClickLink = () => {
+    window.location.href = '/data'
+  }
+
   return (
     <div className='global'>
       {/* Header */}
       <h1><span>y</span>sport</h1>
       <nav className='link'>
-        <a href="/carte-sportif">carte sportif</a>
+        <a href="/data">carte sportif</a>
         <a href="/nouveau-lieu">nouveau lieu</a>
       </nav>
       <div className='social'>
@@ -19,6 +24,10 @@ export default function Home() {
       <main>
       <h2>Votre guide sportif <span>local</span></h2>
       </main>
+      <section className='section_img'>
+        {/* <img src='#' alt='#' /> soon */}
+      </section>
+      <button onClick={onClickLink}>CARTE SPORTIF</button>
     </div>
   )
 }
